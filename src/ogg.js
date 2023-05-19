83c8647622480724ed1ca5ea3176a2287ea3c0c6
sergei <s_udodov@bk.ru>
2023-05-19 22:46:15 +0300

    before deploy

--- src/ogg.js
+++ src/ogg.js
@@ -1,14 +1,36 @@
-import axios from "axios"
-import {createWriteStream} from "fs"
+import axios from 'axios'
+import ffmpeg from 'fluent-ffmpeg'
+import installer from '@ffmpeg-installer/ffmpeg'
+import {createWriteStream} from 'fs'
 import {dirname, resolve} from 'path'
 import {fileURLToPath} from 'url'
+import { removeFile } from './utils.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 class OggConverter {
- constructor(){}
+ constructor(){
+    ffmpeg.setFfmpegPath(installer.path)
+ }
  
- toMp3(){}
+ toMp3(input, output){
+    try {
+        const outputPath = resolve(dirname(input), `${output}.mp3`) 
+        return new Promise((resolve, reject) => {
+            ffmpeg(input)
+                .inputOption('-t 30')
+                .output(outputPath)
+                .on('end', () => {
+                    removeFile(input)
+                    resolve(outputPath)
+                })
+                .on('error', (err) => reject(err.message))
+                .run()
+        })
+    } catch (e) {
+        console.log('Error while creating mp3', e.message)
+    }
+ }
 
  async create(url, filename){
     try {
@@ -18,7 +40,7 @@ class OggConverter {
             url,
             responseType: 'stream',
         })
-        return Promise(resolve => {
+        return new Promise(resolve => {
             const stream = createWriteStream(oggPath)
             response.data.pipe(stream)
             stream.on('finish', () => resolve(oggPath))
